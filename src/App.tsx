c5ff873f91e3198630b39bfac3c715fdab1edc10
franck ledoux <franck.ledoux.pro@gmail.com>
2019-01-07 22:44:01 +0100

    Login layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const theme = {
       family: 'Roboto',
       size: '14px',
       height: '20px',
-    },
+    }
   },
 };
 
--- src/login.tsx
+++ src/login.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState, useCallback } from 'react';
+import { Text, Heading } from 'grommet';
 import { Box } from 'grommet/components/Box';
 import { Layer } from 'grommet/components/Layer';
 import { Button } from 'grommet/components/Button';
@@ -19,17 +20,40 @@ export const Login: React.SFC<{}> = () => {
     [password]);
 
   return (
-    <Layer full plain modal margin="small">
-      <Image src="faker.js" alignSelf="center" />
-      <Box width="small" alignSelf="center" margin="none" align="center" overflow="auto" >
-        <FormField label='Login'>
-          <TextInput onChange={onLoginChange} value={login} />
-        </FormField>
-        <FormField label='Password'>
-          <TextInput type="password" onChange={onPasswordChange} value={password} />
-        </FormField>
-        <Button label="Login" primary disabled={!login || !password} type="submit" margin="xsmall" fill={true} />
-        <Button label="Register" type="submit" margin="xsmall" fill={true} />
+    <Layer full plain modal margin="small" >
+      <Box fill direction="column" background="light-3" align="center" justify="center">
+        <Box direction="column" align="center" justify="center" overflow="auto">
+          {/* <Image src="faker.js" alignSelf="center" /> */}
+          <Heading level="1">Faker Client</Heading>
+
+          <Box
+            direction="column"
+            width="medium"
+            margin={{ top: "medium" }}
+            pad="small"
+            elevation="medium"
+            background="light-1"
+          >
+            <FormField label='Login'>
+              <TextInput onChange={onLoginChange} value={login} />
+            </FormField>
+            <FormField label='Password'>
+              <TextInput type="password" onChange={onPasswordChange} value={password} />
+            </FormField>
+            <Button label="Login" primary margin={{ top: "small" }} />
+          </Box>
+
+          <Box
+            direction="column"
+            width="medium"
+            margin={{ top: "medium" }}
+            pad="small"
+            elevation="medium"
+            background="light-1">
+            <Text>Not registered Yet?</Text>
+            <Button label="Register" margin={{ top: "xsmall" }} />
+          </Box>
+        </Box>
       </Box>
     </Layer >
   );
