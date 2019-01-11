2d0aebfd3b1bec527176d40432f79fdb7991c36f
franck ledoux <franck.ledoux.pro@gmail.com>
2019-01-11 20:38:18 +0100

    First Graphql

--- src/login.tsx
+++ src/login.tsx
@@ -6,7 +6,8 @@ import { Layer } from 'grommet/components/Layer';
 import { Button } from 'grommet/components/Button';
 import { FormField } from 'grommet/components/FormField';
 import { TextInput } from 'grommet/components/TextInput';
-import { Image } from 'grommet/components/Image';
+import axios from 'axios';
+import { log } from 'util';
 
 export const Login: React.SFC<{}> = () => {
   const [login, setLogin] = useState("");
@@ -19,11 +20,15 @@ export const Login: React.SFC<{}> = () => {
     (...args: any[]) => setPassword(args[0].target.value),
     [password]);
 
+  const onRegister = useCallback(
+    (...args: any[]) => register(),
+    [login, password]);
+
+
   return (
     <Layer full plain modal margin="small" >
       <Box fill direction="column" background="light-3" align="center" justify="center">
         <Box direction="column" align="center" justify="center" overflow="auto">
-          {/* <Image src="faker.js" alignSelf="center" /> */}
           <Heading level="1">Faker Client</Heading>
 
           <Box
@@ -51,10 +56,23 @@ export const Login: React.SFC<{}> = () => {
             elevation="medium"
             background="light-1">
             <Text>Not registered Yet?</Text>
-            <Button label="Register" margin={{ top: "xsmall" }} />
+            <Button label="Register" margin={{ top: "xsmall" }} onClick={onRegister} />
           </Box>
         </Box>
       </Box>
     </Layer >
   );
+}
+
+async function register() {
+  const client = axios.create({
+    url: 'https://fakerql.com/graphql'
+  });
+  const foo = await client.post('https://fakerql.com/graphql', {
+    query: `mutation {
+      login(email:"franck.ledoux.progmail.com",password:"foo") {token}
+    }`
+  });
+  console.log('token', foo.data.data.login.token);
+
 }
